3301c720e0df9835e3a0dda298acf2f358191fac
shahrulhaniff <shahrul941013@gmail.com>
2019-03-11 00:26:09 +0800

    release

--- src/providers/global/global.ts
+++ src/providers/global/global.ts
@@ -10,10 +10,10 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class GlobalProvider {
 
-  public mysite : string  = "http://localhost/cashless2/";
+  //public mysite : string  = "http://localhost/cashless2/";
   //public baseURI : string  = "http://192.168.43.194/cashless2/"; 
   //public baseURI : string  = "http://localhost/cashless2/";
-  //public baseURI : string  = "https://raxsoft.000webhostapp.com/cashless2/";
+  public mysite : string  = "https://raxsoft.000webhostapp.com/cashless2/";
 
   constructor(public http: HttpClient) {
     console.log('Hello GlobalProvider Provider');
