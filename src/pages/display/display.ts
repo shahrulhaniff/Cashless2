c11009b584a1977f5dba077668f15ec0367808b1
shahrulhaniff <shahrul941013@gmail.com>
2019-08-18 16:21:58 +0800

    update

--- src/pages/display/display.ts
+++ src/pages/display/display.ts
@@ -86,7 +86,7 @@ export class DisplayPage {
    public jns_da : Array<any> = [];
    loadjenisbayar(): void {
 
-      let url: any = this.baseURI + 'retrieve_idjenistransaksi.php';
+      let url: any = this.baseURI + 'retrieve_idjenistransaksi_semak.php';
 
       this.http.get(url).subscribe((data: any) => {
          console.dir(data);
@@ -126,7 +126,6 @@ export class DisplayPage {
    }
    gotoSenarai2(param: any): void {
       //this.navCtrl.push(SenaraiPage, param);
-
       this.navCtrl.push(SenaraiPage, {
          data: param
       });
